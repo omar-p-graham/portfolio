828f90eb3ac73a4d5812683bc472c37bf813b428
Omar P Graham <142844317+omar-p-graham@users.noreply.github.com>
2025-03-07 01:09:09 -0500

    Completed Desktop Layout

--- js/projects.js
+++ js/projects.js
@@ -1,8 +1,8 @@
 const projects = [
     {
-        "title":"Website: K&H Group",
+        "title":"Website: <span>K&H Group</span>",
         "description":"An assisted living company, offering personalized care of the highest quality and a comfortable, family-like, environment. They provide a range of services to meet the unique needs of each resident.",
-        "tech":"PHP, HTML, CSS, Bootstrap, JavaScript/JQuery",
+        "tech":"PHP, HTML, CSS, Bootstrap, JavaScript/JQuery, SQL",
         "links":{
             "external":"https://www.kandhgroup.org/",
             "github":""
@@ -13,9 +13,9 @@ const projects = [
         }
     },
     {
-        "title":"Website: Tuskin Financial Limited",
+        "title":"Website: <span>Tuskin Financial Limited</span>",
         "description":"A customer driven, friendly and flexible Micro-Financing company, guaranteeing competitive market rates. They offer personal and payroll loans, targeted products for micro, small and medium sized enterprises (SMEs) and the transportation sector.",
-        "tech":"WordPress, PHP, CSS, Bootstrap, JavaScript/JQuery",
+        "tech":"WordPress, PHP, CSS, Bootstrap, JavaScript/JQuery, SQL",
         "links":{
             "external":"https://tuskinholdings.com/",
             "github":""
@@ -26,9 +26,9 @@ const projects = [
         }
     },
     {
-        "title":"Web App: outLaunderX",
-        "description":"A comprehensive anti-money laundering (AML) and Counter Financing of Terrorism (CFT) solution, proving executive level data analysis tailored to meet the unique needs of financial insitutions and regulated entities worldwide.",
-        "tech":"PHP, HTML, CSS, Bootstrap, JavaScript",
+        "title":"Web Application: <span>outLaunderX</span>",
+        "description":"A comprehensive anti-money laundering (AML) and Counter Financing of Terrorism (CFT) solution, providing executive level data analysis tailored to meet the unique needs of financial insitutions and regulated entities worldwide.",
+        "tech":"PHP, HTML, CSS, Bootstrap, JavaScript, SQL",
         "links":{
             "external":"",
             "github":""
@@ -39,9 +39,9 @@ const projects = [
         }
     },
     {
-        "title":"Web App: AML Knowledge Assessment",
+        "title":"Web Application: <span>AML Knowledge Assessment</span>",
         "description":"This application facilitates exam covering the basic requirements and obligations of regulated entities under POCA, TPA and UNSCRIA within Jamaica. The questions focus on key areas of the legislation to ascertain if the employees of the regulated entities have basic knowledge of the requirements of applicable legislation.",
-        "tech":"PHP, HTML, CSS, Bootstrap, JavaScript",
+        "tech":"PHP, HTML, CSS, Bootstrap, JavaScript, SQL",
         "links":{
             "external":"https://aml.fscjamaica.org/knowledge-assessment/",
             "github":""
@@ -52,7 +52,20 @@ const projects = [
         }
     },
     {
-        "title":"Website: Flex Recipe Hub",
+        "title":"Web Application: <span>Flex E-Store</span>",
+        "description":"An easy way to purchase a wide variety products online. With a modern, responsive and dynamic design, this E-commerce application provides excellent shopping experience.",
+        "tech":"Laravel/PHP, Livewire, HTML, CSS, Tailwind, JavaScript, Alpine.js, SQL",
+        "links":{
+            "external":"",
+            "github":"https://github.com/omar-p-graham/ecommerce_laravel"
+        },
+        "image":{
+            "url":"./assets/projects/flexestore.webp",
+            "alt":"Flex E-Store"
+        }
+    },
+    {
+        "title":"Website: <span>Flex Recipe Hub</span>",
         "description":"A culinary voyage with meals from around the world. From the spicy jerk chicken of Jamaica to the sophisticated pastries of France, the ultimate online portal for discovering and mastering international cuisine.",
         "tech":"Vue.js, HTML, CSS, TailWind, JavaScript",
         "links":{
@@ -65,7 +78,7 @@ const projects = [
         }
     },
     {
-        "title":"Website: Flex News",
+        "title":"Website: <span>Flex News</span>",
         "description":"Flex News provides daily news articles at your finger tips on topics like Entertainment, Sports, Science, Technology, among others.",
         "tech":"Vue.js, HTML, CSS, TailWind, JavaScript",
         "links":{
